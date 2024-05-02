144b137ba753ddd720df268979145ec2890952cb
Matt Sayers <matt.sayers@weareswitch.com>
2024-05-02 14:45:45 +0100

    client amends

--- template-strategic-journey.php
+++ template-strategic-journey.php
@@ -1,6 +1,6 @@
 <?php 
 /*
-	Template Name: Bank Raising Project
+	Template Name: Strategic Journey & Projects
 */
 get_header();
 do_action( 'ava_builder_template_after_header' );
@@ -11,6 +11,61 @@ do_action( 'ava_after_main_title' );
 <link rel="stylesheet" href="<?php echo get_stylesheet_directory_uri() ?>/dist/css/style.css">
 <link rel="stylesheet" href="<?php echo get_stylesheet_directory_uri() ?>/dist/css/timeline.css">
 <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
+
+
+<?php 
+    $banner_content = get_field('banner_content');
+    $banner_image = get_field('banner_image');
+?>
+
+<?php if($banner_content || $banner_image) : ?>
+    <div class="theme-block theme-block--banner theme-block--background" data-aos="fade-up">
+        <?php if($banner_image) : ?>
+            <div class="theme-block__background" style="background-image:url('<?php echo $banner_image; ?>'); ?>
+        <?php endif; ?>
+    ');"></div>
+        <?php if($banner_content) : ?>
+        <div class="theme-block__wrapper">
+            <?php echo $banner_content; ?>
+        </div>
+        <?php endif; ?>
+    </div>
+<?php endif; ?>
+
+<?php if( have_rows('timeline_entries') ): ?>
+    <div class="theme-block theme-block--timeline">
+    <div class="theme-block__wrapper">
+        <div class="timeline">
+            <div class="timeline__header">
+                <?php $entryCountPoints = 0; ?>
+                <?php while( have_rows('timeline_entries') ): the_row(); 
+                    $date = get_sub_field('timeline_date');
+                    $entryCountPoints++;
+                    ?>
+                    <div class="input <?php if($entryCountPoints === 1) echo 'active'; ?>">
+                        <div class="input-ball"></div>
+                        <span data-year="<?php echo $date; ?>"></span>
+                    </div>
+                <?php endwhile; ?>
+            </div>
+
+            <div class="timeline__main">
+                <?php $entryCountContent = 0; ?>
+                <?php while( have_rows('timeline_entries') ): the_row(); 
+                    $content = get_sub_field('timeline_content');
+                    $entryCountContent++;
+                    ?>
+                    <div data-count="<?php echo $entryCountContent; ?>" class="timeline__content <?php if($entryCountContent === 1) echo 'active'; ?>"><?php echo $content; ?></div>
+                <?php endwhile; ?>
+            </div>
+        </div>
+    </div>
+    </div>
+<?php endif; ?>
+
+<div class="theme-block theme-block--page-builder">
+    <?php the_content(); ?>
+</div>
 
 <div class="theme-block theme-block--roadmap">
     <div class="theme-block__wrapper">
@@ -115,7 +170,6 @@ do_action( 'ava_after_main_title' );
 
 
 
-
 <script src="<?php echo get_stylesheet_directory_uri() ?>/dist/js/bundle.js"></script>
 
 
@@ -127,6 +181,19 @@ do_action( 'ava_after_main_title' );
 <script>
 
 AOS.init();
+
+$(function(){
+	var inputs = $('.input');
+	var paras = $('.timeline__main').find('.timeline__content');
+	inputs.click(function(){
+		var t = $(this),
+				ind = t.index(),
+				matchedPara = paras.eq(ind);
+		
+		t.add(matchedPara).addClass('active');
+		inputs.not(t).add(paras.not(matchedPara)).removeClass('active');
+	});
+});
 
 let dataPoints = document.getElementsByClassName('roadmap__main-item');
 let modal = document.querySelector('.roadmap-modal');
@@ -158,9 +225,6 @@ modalClose.addEventListener('click', function(){
     modalImage.innerHTML = '';
     modalTitle.innerHTML = '';
 });
-
-
-
 
 </script>
 
